0a02ed6b41682e3a7cd19c45471b77199ddd95b8
jonathanbridges <jbridges7@gmail.com>
2019-12-27 14:04:04 -0800

    Updates

---
+++ precache-manifest.878b45399544bbc1df6cb66a7ef9397e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "55f289e61d936f8db81cfdd894fe465c",
+    "url": "/winter-design-build/index.html"
+  },
+  {
+    "revision": "f61a6e550b48a85c0e70",
+    "url": "/winter-design-build/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2c0301d0c6cd12659661",
+    "url": "/winter-design-build/static/js/2.6a684062.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/winter-design-build/static/js/2.6a684062.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f61a6e550b48a85c0e70",
+    "url": "/winter-design-build/static/js/main.d5e017de.chunk.js"
+  },
+  {
+    "revision": "bd4706e67085b2d25729",
+    "url": "/winter-design-build/static/js/runtime-main.3a40695c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/github.io/winter-design-build/precache-manifest.f3f7998decb901c530c2e9f9fe69c145.js"
+  "/winter-design-build/precache-manifest.878b45399544bbc1df6cb66a7ef9397e.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/github.io/winter-design-build/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/winter-design-build/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
