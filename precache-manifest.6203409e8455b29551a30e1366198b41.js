f9ec869b0010ae34a1cea972fe5e6390bb1c50ac
jonathanbridges <jbridges7@gmail.com>
2019-12-28 12:32:42 -0800

    Updates

---
+++ precache-manifest.6203409e8455b29551a30e1366198b41.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48e1c3ac487a6321a2e6ac9b9daea662",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6e831f46859c11ed8f7b",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "583e40d146228033f455",
+    "url": "/static/js/2.edf302ca.chunk.js"
+  },
+  {
+    "revision": "84a2a7a390df1ac99fa4fadf23155e60",
+    "url": "/static/js/2.edf302ca.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6e831f46859c11ed8f7b",
+    "url": "/static/js/main.d1f8e6ed.chunk.js"
+  },
+  {
+    "revision": "3e37ace3fb878eb2e251",
+    "url": "/static/js/runtime-main.4117b0bf.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/winter-design-build/precache-manifest.59159b36ba78bd7c3e8b5f9aee0b2e44.js"
+  "/precache-manifest.6203409e8455b29551a30e1366198b41.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/winter-design-build/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
