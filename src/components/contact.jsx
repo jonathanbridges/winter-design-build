49db95286746fc83c45588e669c13c69c26833ec
jonathanbridges <jbridges7@gmail.com>
2019-12-28 10:55:04 -0800

    Add email form via formspree

--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,39 +1,44 @@
 import React from 'react';
+import axios from "axios";
 
 export default class Contact extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      message: "",
-      name: "",
-      email: "",
-      subject: "",
+      submitting: false,
+      status: null,
     }
-    this.handleMessageUpdate = this.handleMessageUpdate.bind(this);
-    this.handleNameUpdate = this.handleNameUpdate.bind(this);
-    this.handleEmailUpdate = this.handleEmailUpdate.bind(this);
-    this.handleSubjectUpdate = this.handleSubjectUpdate.bind(this);
+    this.handleServerResponse = this.handleServerResponse.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
   }
 
-  handleMessageUpdate() {
+  handleServerResponse (ok, msg, form) {
+    this.setState({
+      submitting: false,
+      status: { ok, msg },
+      errors: [],
+    });
+    if (ok) {
+      form.reset();
+    }
+  };
 
-  }
-
-  handleNameUpdate() {
-
-  }
-
-  handleEmailUpdate() {
-
-  }
-
-  handleSubjectUpdate() {
-
-  }
-
-  handleSubmit() {
-
-  }
+  handleSubmit(e) {
+    e.preventDefault();
+    const form = e.target;
+    this.setState({ submitting: true });
+    axios({
+      method: "post",
+      url: "https://formspree.io/mgeonelo",
+      data: new FormData(form)
+    })
+      .then(r => {
+        this.handleServerResponse(true, "Thanks for your email!", form);
+      })
+      .catch(r => {
+        this.handleServerResponse(false, r.response.data.error, form);
+      });
+  };
 
   render() {
     return (
@@ -44,31 +49,40 @@ export default class Contact extends React.Component {
               <h2 className="contact-title">Get in Touch</h2>
             </div>
             <div className="col-lg-8">
-              <form className="form-contact contact_form">
+              <form onSubmit={this.handleSubmit} className="form-contact contact_form">
                 <div className="row">
                   <div className="col-12">
                     <div className="form-group">
-                      <textarea className="form-control w-100" name="message" id="message" cols={30} rows={9} placeholder="Enter your message" defaultValue={""} />
+                      <label htmlFor="message"></label>
+                      <textarea className="form-control w-100" name="message" id="message" cols={30} rows={9} placeholder="Enter your message" defaultValue={""} required />
                     </div>
                   </div>
                   <div className="col-sm-6">
                     <div className="form-group">
-                      <input className="form-control valid" name="name" id="name" type="text" placeholder="Enter your name" />
+                      <label htmlFor="name"></label>
+                      <input className="form-control valid" name="name" id="name" type="text" placeholder="Name" required />
                     </div>
                   </div>
                   <div className="col-sm-6">
                     <div className="form-group">
-                      <input className="form-control valid" name="email" id="email" type="email" placeholder="Email" />
+                      <label htmlFor="email"></label>
+                      <input className="form-control valid" name="email" id="email" type="email" placeholder="Email" required />
                     </div>
                   </div>
                   <div className="col-12">
                     <div className="form-group">
-                      <input className="form-control" name="subject" id="subject" type="text" placeholder="Enter Subject" />
+                      <label htmlFor="subject"></label>
+                      <input className="form-control" name="subject" id="subject" type="text" placeholder="Subject" required />
                     </div>
                   </div>
                 </div>
+                {this.state.status && (
+                  <p className={!this.state.status.ok ? "errorMsg" : ""}>
+                    {this.state.status.msg}
+                  </p>
+                )}
                 <div className="form-group mt-3">
-                  <button type="submit" className="button button-contactForm boxed-btn">Send</button>
+                  <button type="submit" className="button boxed-btn" disabled={this.state.submitting}>Send</button>
                 </div>
               </form>
             </div>
